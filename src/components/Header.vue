656b784aecae677ff2543938bb681f70d15549df
rowl <airowl.gasga@gmail.com>
2022-10-08 16:36:28 +0200

    implemente header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,18 +18,18 @@
 
     </nav>
 
-    <div class="input-box" :class='isOpenSearch ? "is-active" : "" '>
-      <input type="text" name="search" id="search">
-      <i class="fa-solid fa-magnifying-glass"></i>
-    </div>
-
     <ul class="menu" :class='isOpenHam ? "is-active" : "" '>
       <li v-for="(e, i) in headerData" :key="i">
-        <a href={e.url}>
+        <a :href="e.url">
           {{e.text}}
         </a>
       </li>
     </ul>
+
+    <div class="input-box" :class='isOpenSearch ? "is-active" : "" '>
+      <input type="text" name="search" id="search">
+      <i class="fa-solid fa-magnifying-glass fa-xs"></i>
+    </div>
 
     
   </header>
@@ -46,7 +46,7 @@ export default {
     return {
         headerData: [
           {
-            text: 'About Us',
+            text: 'About us',
             url: '#',
           },
           {
@@ -87,9 +87,13 @@ export default {
 
 <style lang='scss' scoped>
 header{
-  height: 5rem;
+  height: 4rem;
   width: 100vw;
   position: relative;
+
+  @include breakpoint-up(large){
+    @include d-flex(row, space-between, center);
+  };
 
   nav.navbar{
     height: 100%;
@@ -99,19 +103,36 @@ header{
     background-color: $white;
     @include d-flex(row, space-evenly, center);
 
+    @include breakpoint-up(large){
+      @include d-flex(row, start, center);
+      width: 18vw;
+    };
+
     div#logo{
       text-transform: uppercase;
       background-color: $bg-logo;
-      width: 60vw;
+      width: 62vw;
       height: 100%;
       color: $white;
       @include d-flex(row, center, center);
+      
+      @include breakpoint-up(large){
+        width: 100%;
+      };
+
+      h4{
+        letter-spacing: 4px;
+      }
     }
 
     div.collapse{
       @include d-flex(row, space-evenly, center);
       height: 100%;
       width: 40vw;
+
+      @include breakpoint-up(large){
+        display: none
+      };
 
       .hamburger .line{
         width: 40px;
@@ -155,13 +176,22 @@ header{
     @include d-flex(row, center, center);
     @include transition;
 
+    @include breakpoint-up(large){
+      position: relative;
+      top: 0;
+      width: 20vw;
+      height: 100%;
+      z-index: 5;
+    };
+
     &.is-active{
       top: 5rem;
     }
 
     input#search{
       border: 0;
-      border-bottom: 2px solid black;
+      border-bottom: 1px solid black;
+      width: 6rem;
     }
   }
 
@@ -175,26 +205,42 @@ header{
     height: 100vh;
     z-index: 4;
     background-color: $white;
-    font-family: "Montserrat-b", "sans-serif";
+    font-family: "Montserrat-r", "sans-serif";
     @include d-flex(column, center, center);
     @include transition;
+
+    @include breakpoint-up(large){
+      position: relative;
+      top: 0;
+      width: 60vw;
+      height: 100%;
+      z-index: 5;
+      @include d-flex(row, start, center);
+    };
+    
 
     &.is-active{
       top: 0;
     }
 
     li{
-      margin-bottom: 1rem;
+      margin-bottom: 2rem;
+
+      @include breakpoint-up(large){
+        margin-left: 4rem;
+        margin-bottom: 0;
+      };
 
       a{
         color: $black;
-
         @include transition;
+        letter-spacing: 1px;
 
         &:hover{
           text-decoration: underline;
         }
       }
+
     }
   }
 
